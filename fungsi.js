e11960464cab4a01552285943bc5827d8b32ce7a
Fadriansya <160394556+Fadriansya@users.noreply.github.com>
2024-02-18 18:49:57 +0700

    Add files via upload

---
+++ fungsi.js
@@ -0,0 +1,20 @@
+// CLEAR MESSAGE
+
+//RESPONSIVE (PERTUKARAN POSISI SLIDE)
+const navSlide = () => {
+  const burger = document.querySelector(".burger")
+  const navLists = document.querySelector("nav")
+  
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger")
+  })
+}
+
+navSlide()
+
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+}
